e09231387bd3e6f11c0754967f1919c5a9033bbb
JResearchLabs <jresearchlabs@gmail.com>
2022-10-16 10:25:10 +0530

    adding init validator

---
+++ init_validator.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo yum install git -y
+wget https://golang.org/dl/go1.17.5.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.17.5.linux-amd64.tar.gz
+echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.profile
+source ~/.profile
+git clone https://github.com/JResearchLabs/Flutechain.git
+cd Flutechain
+go build -o Flutechain main.go
+sudo mv Flutechain /usr/local/bin
