06c14ac59e4b9a1bde8bdbf46d8549a2bc8c2374
JResearchLabs <jresearchlabs@gmail.com>
2022-10-23 13:19:25 +0530

    Update init_validator.sh
    removed git install

--- init_validator.sh
+++ init_validator.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-sudo yum install git -y
 wget https://golang.org/dl/go1.17.5.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.17.5.linux-amd64.tar.gz
 echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.profile
